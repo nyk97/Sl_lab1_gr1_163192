4c0f93cd6dd5ec0675478ae507550e41cdd7db24
Nikola Stojkoski LOC <headowned@hotmail.com>
2020-04-12 20:39:47 +0200

    points variable added

--- Student.java
+++ Student.java
@@ -2,18 +2,57 @@ class Student {
 	String index;
 	String firstName;
 	String lastName;
-	int[] labPoints = { 1, 2, 3, 4, 5 };
+	int[] labPoints = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
 
 	//TODO constructor
+	public Student() {
+
+	}
 
 	//TODO seters & getters
+	 // Getter
+	public String getIndex() {
+		return index;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+
+	// Setter
+	public void setIndex(String newIndex) {
+		this.index = newIndex;
+	}
+	public void setFirstName(String newFirstName) {
+		this.firstName = newFirstName;
+	}
+	public void setLastName(String newLastName) {
+		this.lastName = newLastName;
+	}
+
+
 
 	public double getAverage() {
 		//TODO
+		int sum=0;
+		int n = labPoints.length;
+		for(int i = 0; i<n-1;i++) {
+			sum += labPoints[i]
+		}
+		return sum/labPoints.length;
 	}
 
 	public boolean hasSignature() {
 		//TODO
+		if(labPoints.length>=8) {
+			return true;
+		}
+		else {
+			return false;
+		}
+		
 	}
 	
 }
